36d7c4bf8063e63688f60f3768b873e8ccb07d11
Juri <jasonluxie@gmail.com>
2021-09-24 22:38:07 -0500

    cleaned up js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ let gameLoss = 0;
 let startButton = document.querySelector("#start");
 let wordHolder = document.querySelector("#word");
 
-
 let wordArray = ["Banana", "Apple", "Pineapple"];
 
 const guessWord = wordArray[randomInt(wordArray.length)].split("");
@@ -17,18 +16,19 @@ startButton.addEventListener("click", startGame());
 function startGame() {
     let timer = document.querySelector("#timer");
     let time = wordArray.length * 30000;
-    wordGame()
+    wordGame();
     //Timer function
     setInterval(function () {
         if (time > 0) {
             time = time - 1000;
-            // console.log(time);
             let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
             let seconds = Math.floor((time % (1000 * 60)) / 1000);
             timer.textContent = minutes + ":" + seconds;
         }
         if (time <= 0) {
-            var playAgain = confirm("You ran out of time! Do you want to play again?")
+            var playAgain = confirm(
+                "You ran out of time! Do you want to play again?"
+            );
             if (playAgain == true) {
                 time = wordArray.length * 30000;
             }
@@ -45,14 +45,14 @@ function wordGame() {
     for (i = 0; i <= guessWord.length - 1; i++) {
         function letterBox() {
             let letterDiv = document.createElement("div");
-            letterDiv.classList.add("letter")
+            letterDiv.classList.add("letter");
             return letterDiv;
         }
         wordHolder.appendChild(letterBox());
     }
 }
 
-let letterHolder = wordHolder.children
+let letterHolder = wordHolder.children;
 function letterGuess(event) {
     let key = event.key.toLowerCase();
     //Goes through every index of the array guessWord[]
